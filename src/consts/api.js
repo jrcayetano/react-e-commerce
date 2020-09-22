fc75ebaa1cac4e7229fdec0760de18ea04ce4178
josercayetano <josercayetano@gmail.com>
2020-09-22 09:03:42 +0200

    added consts folder

---
+++ src/consts/api.js
@@ -0,0 +1,9 @@
+export const API_PRODUCTS = "products";
+export const API_LOGIN = "login";
+export const API_STATES = "states";
+// USER PATH
+export const API_USER = "user";
+export const API_PROFILE = "userprofile";
+export const API_PROFILE_EDIT = "userprofileedit";
+export const API_USER_ORDERS = "userorders";
+export const API_FAVORITE_PRODUCTS = "userfavorites";
